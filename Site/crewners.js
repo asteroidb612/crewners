15ea58190295596f2d113c4522c0617e1dc83215
Drew Lazzeri <dlazzeri1@gmail.com>
2014-03-13 21:29:20 -0700

    Blur! kinda

--- Site/crewners.js
+++ Site/crewners.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){ 
   setTimeout(function() {
     $("#menu").hide().fadeIn(1000);
-
+    $("html").css({"background": "url('images/homepage_blur.jpg') no-repeat top center fixed",
+   "-webkit-background-size": "cover",
+   "-moz-background-size": "cover",
+   "-o-background-size": "cover", 
+   "background-size": "cover"});
   }, 500);
 })
